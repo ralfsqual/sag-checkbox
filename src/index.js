e3baa1d838ab55f57371de3126fe693e4dc80c56
wangyonglin <ralfsqual@aliyun.com>
2019-05-23 10:42:38 +0800

    去除测试用的一些提示

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import SagCheckbox from "./components/SagCheckbox";
 const SagCheckboxPlugin = {
     install(Vue/*,options*/){
         Vue.component(SagCheckbox.name,SagCheckbox);
-        console.log(SagCheckbox.name +' is registered')
     }
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(SagCheckboxPlugin);
-    console.log('SagCheckboxPlugin is used')
 
 }
 export default SagCheckboxPlugin;
